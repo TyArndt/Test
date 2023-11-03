71dbd87bccf94f0ba755a15783da81e0967367d3
Tyler Arndt <32685035+TyArndt@users.noreply.github.com>
2023-11-03 16:38:57 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+mmodule "kafka_instance" {
+  source = "cloudposse/ec2-instance/aws?ref=v0.50.0"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version     = "x.x.x"
+  ssh_key_pair                = "test"
+  vpc_id                      = var.vpc_id
+  security_groups             = var.security_groups
+  subnet                      = var.subnet
+  associate_public_ip_address = true
+  name                        = "kafka"
+  namespace                   = "eg"
+  stage                       = "dev"
+  additional_ips_count        = 1
+  ebs_volume_count            = 2
+  security_group_rules = [
+    {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 65535
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      from_port   = 53
+      to_port     = 53
+      protocol    = "udp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+  ]
+}
